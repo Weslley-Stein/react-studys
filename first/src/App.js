19fd9973430880d571fd624e6bffcdfba56411f3
Weslley Stein <weslley.stein@gmail.com>
2022-06-24 00:18:37 +0000

    setup

---
+++ first/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>Because i want to become riiiiich bitch</li>
+        <li>Oh yeah i will be fucking gorgeous, shredded, handsome oh yeah a piece of gold</li>
+        <li>And for that i do need money</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
